52ddebfebe75cff670770c388b4e98e93e3558ef
Михаил Борисенков <mixa_borisenkov@mail.ru>
2026-02-19 12:15:38 +0300

    first prog v3

--- get-dac/8-bit-dac-manual.py
+++ get-dac/8-bit-dac-manual.py
@@ -8,7 +8,8 @@ GPIO.output(leds, 0)
 
 dynamic_range = 3.3
 
-
+def number_to_dac(value):
+    return [int(element) for element in bin(value)[2:].zfill(8)]
 
 def voltage_to_number(voltage):
     if not (0.0 <= voltage <= dynamic_range):
